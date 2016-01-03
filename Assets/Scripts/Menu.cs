fd1295115107217352fa79bbfeb1a865bc7a11dd
gn1204 <m2007365@live.tees.ac.uk>
2016-01-03 23:52:04 +0000

    Added Main menu and an awesome sound

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public Canvas quitMenu;
+	public Button playGame;
+	public Button exitGame;
+	public GameObject playButton;
+	public GameObject yesButton;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+		quitMenu = quitMenu.GetComponent<Canvas> ();
+		playGame = playGame.GetComponent<Button> ();
+		exitGame = exitGame.GetComponent<Button> ();
+
+		quitMenu.enabled = false;
+	}
+
+	public void PlayClick()
+	{
+		Application.LoadLevel (1);
+		
+	}
+
+	public void ExitClick()
+	{
+		quitMenu.enabled = true;
+		playGame.enabled = false;
+		exitGame.enabled = false;
+		EventSystem.current.SetSelectedGameObject (yesButton);
+	
+	}
+
+	public void YesClick()
+	{
+		Application.Quit ();
+		
+	}
+
+	public void NoClick()
+	{
+		quitMenu.enabled = false;
+		playGame.enabled = true;
+		exitGame.enabled = true;
+
+		EventSystem.current.SetSelectedGameObject (playButton);
+
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
